f1a61d045cc2b58f1f09eceae15737b570f239ad
İdris Aktaş <akta.idris@gmail.com>
2025-03-03 21:57:30 +0300

    commit all train

---
+++ js-temeller/app.js
@@ -0,0 +1,54 @@
+/*
+#tek satır if else şartı
+
+let yas = 20;
+let mesaj = yas >= 18 ? "ehliyet albilir" : "ehliyet alamaz";
+console.log(mesaj);
+*/
+
+/*
+# type of
+
+let obj = {
+  name: "idris",
+  lastname: "aktas",
+};
+
+console.log(typeof 5);
+console.log(typeof "5");
+console.log(typeof true);
+console.log(typeof obj);
+*/
+/*
+console.log(5 > 30 && 8 > 2); // false and koşulu
+console.log(5 > 30 || 8 > 2); // true  or koşulu
+
+let üye = {
+  age: 18,
+  member: true,
+};
+
+let result = üye.age && üye.member ? "giriş başarılı" : "giriş başarısız";
+console.log(result);
+*/
+let user = {
+  username: "idrisaktaas",
+  password: "4523",
+  log: false,
+};
+while (!user.log) {
+  let logusername = prompt("User Name:");
+  let loguserpassword = prompt("Password");
+
+  console.log("obj.username" + typeof user.username);
+  console.log("obj.password" + typeof user.password);
+  console.log("logusername" + typeof logusername);
+  console.log("logpassword" + typeof loguserpassword);
+
+  if (user.username == logusername && user.password == loguserpassword) {
+    console.log("Giriş Başarılı.");
+    user.log = true;
+  } else {
+    alert("Giriş Başarısız. kullanıcı adı veya şifreniz hatalı");
+  }
+}

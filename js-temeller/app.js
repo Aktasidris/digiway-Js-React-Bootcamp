f5ade7ac07a7e1544f9b71f5aaa1d5ff0ddd1dd3
Aktasidris <akta.idris@gmail.com>
2025-03-05 16:58:27 +0300

    operatörler, karşılaştırmalar ve bigint konusu öğrenildi

--- js-temeller/app.js
+++ js-temeller/app.js
@@ -1,3 +1,17 @@
+document.write("<h1>Tarayıcı çalıştı çıktıyı görmek için consolu kontrol et");
+/**# data types
+
+let x1 = "";             // new primitive string
+let x2 = 0;              // new primitive number
+let x3 = false;          // new primitive boolean
+const x4 = {};           // new object
+const x5 = [];           // new array object
+const x6 = /()/;         // new regexp object
+const x7 = function(){}; // new function object
+"var"değişken tipi artık kullanılmıyor onun yerine "let" kullanılır 
+eğer değişken sabit bırakılıp daha yeni atama ile değiştirilmesi önlenmek isteniyorsa "const" kullanılır. 
+*/
+
 /*
 #tek satır if else şartı
 
@@ -85,3 +99,25 @@ switch (
     console.log("yaşlı");
 }
 */
+/**
+  -, *, /, % gibi işlemler yapıldığında stringler number'a çevrilir.
++ operatörü varsa en az biri string ise birleştirme yapılır. 
+*/
+let x = "string";
+console.log(typeof x);
+x = 5;
+y = "4";
+console.log(x + y); //54 string
+console.log(x - y); //1 number
+console.log(0 == "       "); //true ama string veya 0 dışında sayı gelirse false olur
+
+//0.1 ve 0.2 ikili sistemde sonsuz tekrar eden diziler olarak saklanır.
+let x2 = 0.1;
+let y2 = 0.2;
+let z = x2 + y2; //toFixed() bir string döndürür, eğer sayı olarak kullanacaksanız parseFloat()
+console.log(z); //0.30000000000000004
+/**
+ * iki değerin türlerini zorla eşitleyerek karşılaştırma yapar.
+ * Yani type coercion (tip dönüşümü) gerçekleştirir.
+ *
+ */
